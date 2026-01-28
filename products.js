30cb11841144e4b7e6a57ae626a59efee2c9d15c
nad-15 <123393892+nad-15@users.noreply.github.com>
2026-01-28 22:53:48 +0800

    Initial clean commit of Express project

---
+++ products.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+router.get(`/`, (req, res) => {
+  res.json([
+    { id: 1, name: `Laptop`, price: 1299 },
+    { id: 2, name: `Mouse`, price: 50 }
+  ]);
+});
+
+router.get(`/:id`, (req, res) => {
+  const id = Number(req.params.id);
+
+  const products = [
+    { id: 1, name: `Laptop`, price: 1299 },
+    { id: 2, name: `Mouse`, price: 50 }
+  ]
+
+  const requestedProduct = products.find((product) => product.id === id)
+  res.json(requestedProduct);
+
+});
+
+router.post(`/`, (req, res)=> {
+    const {name, price} = req.body;
+    const newProduct = {
+        name,
+        price
+    }
+    console.log(newProduct);
+    res.json({message: "New prodcut added", product: newProduct});
+
+});
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,49 @@
+const cors = require(`cors`);
+const express = require('express');
+const productsRouter = require(`./products`);
+const app = express();
+
+
+const port = 3000;
+
+app.use(cors({
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500']
+}));
+
+app.use(express.json());
+
+app.use(`/products`, productsRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Us');
+});
+
+app.get(`/contact`, (req, res) => {
+  res.send(`Contact us`);
+});
+
+
+
+app.get(`/message`, (req, res) => {
+  res.json({
+    message: `Hello from Express backend!`,
+    text: `Hi from text not message key`
+  })
+});
+
+app.post(`/message`, (req, res) => {
+  const { name, message } = req.body;
+
+  console.log(`New Message: `, name, message);
+  res.json({ message: `Thank you for the message` });
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
